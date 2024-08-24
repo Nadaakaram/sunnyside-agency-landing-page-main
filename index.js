cadba997f73dcb830b0bfc3638624e931134502a
Nadaakaram <nadakkaram@gmail.com>
2024-08-24 07:05:20 +0300

    edit

---
+++ index.js
@@ -0,0 +1,13 @@
+// var n = window.matchMedia('(max-width:375px)');
+
+// mediaFunction(n);
+
+// n.addEventListener('change', function(){mediaFunction(n)});
+
+
+// function mediaFunction(n) {
+//     if (n.matches){
+//         document.getElementsByClassName('first-img')[0].style.display = 'none';
+//         document.getElementsByClassName('first-img-1')[0].style.display = 'block';
+//     }
+// }
